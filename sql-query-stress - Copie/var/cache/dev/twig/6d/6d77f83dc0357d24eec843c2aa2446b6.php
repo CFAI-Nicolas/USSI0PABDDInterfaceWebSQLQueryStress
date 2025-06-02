24078cad893c45e273eb1a28e208833d78f29e07
CFAI-Nicolas <nremy@cfai-formation.fr>
2025-06-02 12:01:17 +0200

    prob

---
+++ sql-query-stress - Copie/var/cache/dev/twig/6d/6d77f83dc0357d24eec843c2aa2446b6.php
@@ -0,0 +1,159 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* main/index.html.twig */
+class __TwigTemplate_d43542100baea7ca2d4938aa5b29c68e extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context): bool|string|Template|TemplateWrapper
+    {
+        // line 1
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "main/index.html.twig"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "main/index.html.twig"));
+
+        $this->parent = $this->load("base.html.twig", 1);
+        yield from $this->parent->unwrap()->yield($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+    }
+
+    // line 3
+    /**
+     * @return iterable<null|scalar|\Stringable>
+     */
+    public function block_body(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 4
+        yield "<h1>Connexion SQL Server</h1>
+<form method=\"post\" action=\"";
+        // line 5
+        yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("connect_sql");
+        yield "\">
+    <label>Serveur :</label>
+    <input type=\"text\" name=\"host\" value=\"sqlserver\" required><br>
+
+    <label>Port :</label>
+    <input type=\"number\" name=\"port\" value=\"1433\" required><br>
+
+    <label>Base de données :</label>
+    <input type=\"text\" name=\"database\" value=\"TestStress\" required><br>
+
+    <label>Utilisateur :</label>
+    <input type=\"text\" name=\"username\" value=\"sa\" required><br>
+
+    <label>Mot de passe :</label>
+    <input type=\"password\" name=\"password\" required><br>
+
+    <button type=\"submit\">Connexion</button>
+</form>
+";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "main/index.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  79 => 5,  76 => 4,  63 => 3,  40 => 1,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("{% extends 'base.html.twig' %}
+
+{% block body %}
+<h1>Connexion SQL Server</h1>
+<form method=\"post\" action=\"{{ path('connect_sql') }}\">
+    <label>Serveur :</label>
+    <input type=\"text\" name=\"host\" value=\"sqlserver\" required><br>
+
+    <label>Port :</label>
+    <input type=\"number\" name=\"port\" value=\"1433\" required><br>
+
+    <label>Base de données :</label>
+    <input type=\"text\" name=\"database\" value=\"TestStress\" required><br>
+
+    <label>Utilisateur :</label>
+    <input type=\"text\" name=\"username\" value=\"sa\" required><br>
+
+    <label>Mot de passe :</label>
+    <input type=\"password\" name=\"password\" required><br>
+
+    <button type=\"submit\">Connexion</button>
+</form>
+{% endblock %}
+", "main/index.html.twig", "/var/www/html/templates/main/index.html.twig");
+    }
+}
